79dc79d144e36d2f0d8c08d848a31398dea6400b
KSenthilkumar2024 <163306272+KSenthilkumar2024@users.noreply.github.com>
2024-08-08 17:35:42 +0530

    log tested

--- libraries/AP_Motors/AP_MotorsTailsitter.cpp
+++ libraries/AP_Motors/AP_MotorsTailsitter.cpp
@@ -323,8 +323,7 @@ void AP_MotorsTailsitter::output_armed_stabilizing()
     AP::logger().Write("CUST", "pitch_adjustment_gain", "f",
                                         pitch_adjustment_gain);
 
-    AP::logger().Write("CUST1", "vectexponent", "f",
-                                            vectexponent);
+    //AP::logger().Write("CUST1", "vectexponent", "f",vectexponent);
 }
 
 // output_test_seq - spin a motor at the pwm value specified
