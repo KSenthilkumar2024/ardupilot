899b981fe67cb5efc0788ee172230efad33608d6
KSenthilkumar2024 <163306272+KSenthilkumar2024@users.noreply.github.com>
2024-08-08 16:33:29 +0530

    log

--- libraries/AP_Motors/AP_MotorsTailsitter.cpp
+++ libraries/AP_Motors/AP_MotorsTailsitter.cpp
@@ -24,11 +24,10 @@
 #include <GCS_MAVLink/GCS.h>
 #include <SRV_Channel/SRV_Channel.h>
 #include <AP_Logger/AP_Logger.h>
-#include "AP_Logger/LogStructure.h"
+
 
 extern const AP_HAL::HAL& hal;
 
-extern AP_Logger *logger;
 
 #define SERVO_OUTPUT_RANGE  4500
 
@@ -294,8 +293,7 @@ void AP_MotorsTailsitter::output_armed_stabilizing()
      AP::logger().Write("CUST", "pitch_adjustment_gain", "f",
                                         pitch_adjustment_gain);
 
-    AP::logger().Write("CUST1", "vectexponent", "f",
-                                            vectexponent);
+    //AP::logger().Write("CUST1", "vectexponent", "f",vectexponent);
 }
 
 // output_test_seq - spin a motor at the pwm value specified
