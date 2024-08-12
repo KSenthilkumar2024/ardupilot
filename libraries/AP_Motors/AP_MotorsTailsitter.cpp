a7f8cded091258b0f81974b47194b4ef199a02f9
KSenthilkumar2024 <163306272+KSenthilkumar2024@users.noreply.github.com>
2024-08-12 12:43:11 +0530

    Pitch thrust and adjustment gains are multiplied

--- libraries/AP_Motors/AP_MotorsTailsitter.cpp
+++ libraries/AP_Motors/AP_MotorsTailsitter.cpp
@@ -245,7 +245,7 @@ void AP_MotorsTailsitter::output_armed_stabilizing()
         pitch_adjustment_gain = powf(pitch_out,vectexponent); //New expo is named as _pitch_compensation_gain
         pitch_thrust = 1.0f;
     }else if (pitch_out < -1.0f){
-        pitch_adjustment_gain = powf(pitch_out,vectexponent);
+        pitch_adjustment_gain = powf(fabsf(pitch_out),vectexponent);
         pitch_thrust = -1.0f;
     }else{
         pitch_adjustment_gain = 1.0; //pitch_thrust * powf(compensation_gain, _vec_exponent);
@@ -278,8 +278,8 @@ void AP_MotorsTailsitter::output_armed_stabilizing()
     _thrust_right = throttle_thrust - roll_thrust * 0.5f;*/
 
     // calculate left and right throttle outputs  for compensating pitch
-    _thrust_left  = (throttle_thrust + (roll_thrust * 0.5f)) * pitch_adjustment_gain; 
-    _thrust_right = (throttle_thrust - (roll_thrust * 0.5f)) * pitch_adjustment_gain;
+    _thrust_left  = throttle_thrust + (roll_thrust * 0.5f) + pitch_thrust * pitch_adjustment_gain; 
+    _thrust_right = throttle_thrust - (roll_thrust * 0.5f) - pitch_thrust * pitch_adjustment_gain;
 
     thrust_max = MAX(_thrust_right,_thrust_left);
     thrust_min = MIN(_thrust_right,_thrust_left);
