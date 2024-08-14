cf83617c4a03f2951e1b1febbb0b406a213159a5
KSenthilkumar2024 <163306272+KSenthilkumar2024@users.noreply.github.com>
2024-08-14 17:48:05 +0530

    Motor cus command to write the log

--- ArduPlane/quadplane.h
+++ ArduPlane/quadplane.h
@@ -206,13 +206,15 @@ private:
 
     // Initialise motors to allow passing it to tailsitter in its constructor
     AP_MotorsMulticopter *motors = nullptr;
-    //AP_MotorsTailsitter *motcus = nullptr;   //structurelog
+    
     const struct AP_Param::GroupInfo *motors_var_info;
 
     AC_AttitudeControl_Multi *attitude_control;
     AC_PosControl *pos_control;
     AC_WPNav *wp_nav;
     AC_Loiter *loiter_nav;
+    
+    AP_MotorsTailsitter *motcus = nullptr;   //structurelog
     
     // maximum vertical velocity the pilot may request
     AP_Float pilot_speed_z_max_up;
