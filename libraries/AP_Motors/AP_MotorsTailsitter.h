71065f3d3d4d48b383696468bd83d043f154ac87
KSenthilkumar2024 <163306272+KSenthilkumar2024@users.noreply.github.com>
2024-08-20 17:46:22 +0530

    new c1

--- libraries/AP_Motors/AP_MotorsTailsitter.h
+++ libraries/AP_Motors/AP_MotorsTailsitter.h
@@ -32,7 +32,7 @@ public:
 
     // Set by tailsitters using diskloading minumum outflow velocity limit
     void set_min_throttle(float val) {_external_min_throttle = val;}
-s
+
     #if HAL_LOGGING_ENABLED
     // 10hz logging of voltage scaling and max trust
     void                Log_Write() override;       //structurelog
