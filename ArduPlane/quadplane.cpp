ca72ce844e05e1b07347ceb72d49859f81e3fb22
KSenthilkumar2024 <163306272+KSenthilkumar2024@users.noreply.github.com>
2024-08-21 10:26:17 +0530

    log

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -704,7 +704,7 @@ bool QuadPlane::setup(void)
     }
     
     if (hal.util->available_memory() <
-        4096 + sizeof(*motors) + sizeof(*attitude_control) + sizeof(*pos_control) + sizeof(*wp_nav) + sizeof(*ahrs_view) + sizeof(*loiter_nav) + sizeof(*weathervane) + sizeof(*motcus)) { //structurelog
+        4096 + sizeof(*motors) + sizeof(*attitude_control) + sizeof(*pos_control) + sizeof(*wp_nav) + sizeof(*ahrs_view) + sizeof(*loiter_nav) + sizeof(*weathervane)) { //structurelog
         AP_BoardConfig::config_error("Not enough memory for quadplane");
     }
 
