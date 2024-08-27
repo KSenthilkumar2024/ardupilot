b296be3a000879ee3b2b7e19203fe4c23771b1d9
KSenthilkumar2024 <163306272+KSenthilkumar2024@users.noreply.github.com>
2024-08-27 14:42:16 +0530

    LOg on August 27

--- libraries/AP_Logger/LogStructure.h
+++ libraries/AP_Logger/LogStructure.h
@@ -672,7 +672,7 @@ struct PACKED log_Cust {  //structurelog
       float vexp;           
       float pit_adj;  
       float pit_pboo; 
-      float pit_org;  
+      //float pit_org;  
       //float thr_lstk;      
       //float thr_rstk;      
 };      
@@ -1238,7 +1238,7 @@ struct PACKED log_VER {
 // @Field: Vexpo: Exponent term for customized 
 // @Field: PitAdj: adjustment customized gain
 // @Field: PitPBo: Boost customized gain 
-// @Field: PitOrg: Boost customized gain 
+
 
 //  messages for all boards
 #define LOG_COMMON_STRUCTURES \
@@ -1368,7 +1368,7 @@ LOG_STRUCTURE_FROM_AIS \
     { LOG_MOTBATT_MSG, sizeof(log_MotBatt), \
       "MOTB", "QfffffB",  "TimeUS,LiftMax,BatVolt,ThLimit,ThrAvMx,ThrOut,FailFlags", "s------", "F------" , true }, \
     { LOG_CUST_MSG, sizeof(log_Cust),\
-     "CUST", "Qfffffffff", "TimeUS,ThrLeft,ThrRight,RllThr,PitThr,YawThr,Vexpo,PitAdj,PitPBo,PitOrg", "s---------", "F---------" , true }  //structurelog
+     "CUST", "Qffffffff", "TimeUS,ThrLeft,ThrRight,RllThr,PitThr,YawThr,Vexpo,PitAdj,PitPBo", "s--------", "F--------" , true }  //structurelog
 
 
 
